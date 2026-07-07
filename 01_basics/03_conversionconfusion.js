bc26b9005945a4cb34e58ac7e17cdd0e29ddfa32
shuklaharshit090-droid <shuklaharshit090@gmail.com>
2026-07-07 14:13:57 +0000

    conversionconfusing

---
+++ 01_basics/03_conversionconfusion.js
@@ -0,0 +1,27 @@
+let score=undefined
+// let score=null
+// let score="33abc"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN //because the values written cannot be changed
+// true => 1; false=>0
+
+let isLoggedIn="Harshit"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false 
+//""=false
+// "hitesh"=> true
+
+let someNumber=33
+let a=String(someNumber)
+console.log(a);
+console.log(typeof a)
