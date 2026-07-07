f32c590682a27467e18fffe14f74d00d7ef8865a
shuklaharshit090-droid <shuklaharshit090@gmail.com>
2026-07-07 13:24:22 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all JS code as never version
+// alert(3+3) // we are using nodejs,not browser so gives error
+console.log(3+3)
+let name="hitesh"
+let age = 18
+let isLoggedIn = false
+let a;//gives undefined
+// let a=null; // assign null value
+
+// primitive dt-
+//number => 2 to the power 53
+// bigint
+//string- can be used in single or double quoutes
+// boolean- true or false
+// null- both datatype and standalone value in javascript
+//undefined =>
+// symbol
+
+// non primitive-
+
+//object
+console.log(typeof "Hitesh")
+console.log(typeof null)     // give object
+console.log(typeof undefined) // gives undefined
