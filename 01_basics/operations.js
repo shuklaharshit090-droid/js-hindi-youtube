b16d4ee5c3b3a253305019cb4bfb4ae6fc433308
shuklaharshit090-droid <shuklaharshit090@gmail.com>
2026-07-11 13:31:26 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,30 @@
+let value=3
+let negValue=-value
+// console.log(negValue);
+// console.log(2**2);//2 to the power 2
+// console.log(2**3); //2 to the power 3
+
+// let str1="hello "
+// let str2="hitesh"
+
+// let str3=str1 + str2
+// console.log(str3);
+// console.log("1"+ 2);
+// console.log(1+"2");
+// console.log("1"+2+2);// gives 122 beacause first string is encounterd.
+// console.log(1+2+"2");//gives 32  because first number is encountered.
+// // hello hitesh
+// // 12
+// // 12
+// // 122
+// // 32
+
+// console.log(+true); // gives 1
+// console.log(true);  // gives true
+
+let num1,num2,num3
+num1=num2=num3=4
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
