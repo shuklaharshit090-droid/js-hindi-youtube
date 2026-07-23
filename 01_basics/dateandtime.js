a784fed59abe42fac3077c62598ed877a370f781
Harshit Shukla <shuklaharshit090@gmail.com>
2026-07-23 16:51:22 +0000

    dateandtime

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,26 @@
+// let myDate= new Date()
+// // console.log(myDate);
+// // 2026-07-23T16:19:27.784Z
+// // console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23,5,3)// months begin from 0
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate=new Date("01-14-2023")
+// let myTimeStamp=Date.now()
+// // console.log(myTimeStamp);// returns in millisecond from given date
+// // console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate=new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+
+// // `${newDate.getDay()} and the time
+
+// newDate.toLocaleString('default',{
+//     weekday:"long",
+// })
