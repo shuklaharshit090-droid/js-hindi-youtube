312d480e88b3a5423936dc2381a416d8418c0a5f
Harshit Shukla <shuklaharshit090@gmail.com>
2026-08-03 13:15:13 +0000

    JSON

---
+++ 02_basics/object4.js
@@ -0,0 +1,29 @@
+const course = {
+    coursename: "js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+// console.log(course.courseInstructor);
+
+const{courseInstructor: instructor} = course
+console.log(instructor);
+
+// const navbar= ({company}) => {
+
+// }
+
+// navbar(company="hitesh");
+
+//APIS
+//json - proper json structure consist both key and values in the form of string
+// {
+//     name:"hitesh",
+//     coursename:"js in hindi",
+//     price:"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
