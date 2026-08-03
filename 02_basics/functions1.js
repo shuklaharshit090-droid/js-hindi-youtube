3e19c0d0029602043a471ba86e72ae8ddaac0be3
Harshit Shukla <shuklaharshit090@gmail.com>
2026-08-03 14:00:37 +0000

    function

---
+++ 02_basics/functions1.js
@@ -0,0 +1,25 @@
+// console.log("H")
+// console.log("A")
+//  console.log("R")
+//   console.log("S")
+//    console.log("H")
+
+function sayMyName(){
+console.log("H")
+console.log("A")
+console.log("R")
+console.log("S")
+console.log("H")
+}
+// sayMyName()
+function addTwoNumbers(number1,number2){//parameter{
+    return number1+number2
+}
+// addTwoNumbers(5,4)//9//arguments
+// addTwoNumbers(5,"4")//54
+// addTwoNumbers(5,"a")//5a
+
+const result=addTwoNumbers(3,5)
+console.log(result)
+
+
