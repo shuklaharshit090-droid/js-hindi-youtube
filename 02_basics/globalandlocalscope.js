50e52ab2cadf05f5fe27f497a6a3876096dbb267
Harshit Shukla <shuklaharshit090@gmail.com>
2026-08-05 12:54:07 +0000

    scopeinjs

---
+++ 02_basics/globalandlocalscope.js
@@ -0,0 +1,17 @@
+// let a = 10
+// const b = 20
+// var c=30
+let a=300
+if(true){
+  let a = 10
+const b = 20
+var c=30
+console.log("INNER:",a);
+}
+console.log(a)
+// console.log(b)
+// console.log(c)
+for(let i=0;i<Array.length;i++){
+    const element=array[i];
+}
+console.log(a);
